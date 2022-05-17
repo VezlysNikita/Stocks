b075e7e8282f5d1aa559ce5013f4e3184945c6d4
VezlysNikita <telnovasnikita@gmail.com>
2022-05-17 18:42:05 +0300

    visi failai

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,224 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#sendmail').addEventListener('click', send_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email(data) {
+
+  console.log(data)
+
+  if (data.recipients == undefined) {
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
+
+  }
+
+  else {
+    document.querySelector('#compose-recipients').value = data.sender;
+    document.querySelector('#compose-subject').value = 'Re: ' + data.sender;
+    document.querySelector('#compose-body').value = '';
+  }
+
+  // Show compose view and hide other views
+
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email').style.display = 'none';
+
+  // Clear out composition fields
+
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch('/emails/'+ mailbox)
+  .then(response => response.json())
+  .then(emails => {
+
+      // Print emails
+
+
+      for (let i = 0; i < emails.length; i++) {
+        const email_div = document.createElement('div');
+        email_div.classList.add('email');
+
+        email_div.innerHTML = `<div>Subject: ${emails[i].subject}</div><div>From: ${emails[i].sender}</div><div>Date: ${emails[i].timestamp}</div>`;
+
+        document.querySelector('#emails-view').append(email_div);
+
+
+        email_div.addEventListener('click', () => load_email(emails[i].id));
+        
+        if(emails[i].read == true) {
+
+          email_div.style.backgroundColor = 'grey';
+    
+        }
+
+        else {
+          email_div.style.backgroundColor = 'white';
+        }
+
+
+        if (mailbox == 'inbox') {
+
+        
+          const archived_btn = document.createElement('input');
+
+          archived_btn.setAttribute('type','submit')
+          
+          archived_btn.setAttribute('value','Archive')
+
+          archived_btn.classList.add('archivedbtn');
+
+          document.querySelector('#emails-view').append(archived_btn);
+
+          archived_btn.addEventListener('click', () => {
+
+            fetch(`emails/${emails[i].id}`,{
+              method: 'PUT',
+
+              body: JSON.stringify({
+                archived: true
+              })
+            })
+            location.reload();
+          });
+        }
+
+        
+        if (mailbox == 'archive') {
+       
+          const unarchived_btn = document.createElement('input');
+
+          unarchived_btn.setAttribute('type','submit')
+          
+          unarchived_btn.setAttribute('value','Unarchive')
+
+          unarchived_btn.classList.add('unarchivedbtn');
+
+          document.querySelector('#emails-view').append(unarchived_btn);
+
+          unarchived_btn.addEventListener('click', () => {
+            fetch(`emails/${emails[i].id}`,{
+              method: 'PUT',
+
+              body: JSON.stringify({
+                archived: false
+              })
+            })
+            location.reload();
+          });
+
+
+        }
+        
+      }
+
+
+
+  });
+
+}
+
+
+function load_email(id) {
+
+
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+
+
+  fetch(`emails/${id}`)
+  .then(response => response.json())
+  .then(emailas => {
+
+
+
+    //const inemail_div = document.createElement('div');
+    //inemail_div.classList.add('inemail');
+
+    document.querySelector('#email').innerHTML = `<div>Subject: ${emailas.subject}</div><div>From: ${emailas.sender}</div><div>Date: ${emailas.timestamp}</div><div>${emailas.body}</div>`;
+    //document.querySelector('#email').append(inemail_div);
+
+    /*
+    const replybtn = document.createElement('input');
+
+    replybtn.setAttribute('type','submit')
+    
+    replybtn.setAttribute('value','Reply')
+
+    replybtn.classList.add('reply');
+
+    document.querySelector('#email').append(replybtn);
+
+    
+    */
+
+    let reply = document.createElement("BUTTON");
+    reply.innerHTML = "Reply";
+    document.querySelector('#email').append(reply)
+    reply.addEventListener('click', () => compose_email(emailas));
+
+  });
+
+  fetch(`emails/${id}`,{
+    method: 'PUT',
+
+    body: JSON.stringify({
+      read: true
+    })
+
+  })
+}
+
+function send_email(e) {
+
+  e.preventDefault()
+
+  const recipient = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  const photo = document.querySelector('#compose-photo').value;
+
+  console.log(recipient)
+  console.log(subject)
+  console.log(body)
+  console.log(photo)
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipient,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+
+
+  
+}
---
+++ mail/views.py
@@ -0,0 +1,125 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import User, Post
+
+
+def index(request):
+
+    # Authenticated users view their inbox
+    if request.user.is_authenticated:
+        return render(request, "mail/inbox.html")
+
+    # Everyone else is prompted to sign in
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+
+@csrf_exempt
+@login_required
+def compose(request):
+
+    # Composing a new email must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # Check recipient emails
+    data = json.loads(request.body)
+    emails = [email.strip() for email in data.get("recipients").split(",")]
+    if emails == [""]:
+        return JsonResponse({
+            "error": "At least one recipient required."
+        }, status=400)
+
+    # Convert email addresses to users
+    recipients = []
+    for email in emails:
+        try:
+            user = User.objects.get(email=email)
+            recipients.append(user)
+        except User.DoesNotExist:
+            return JsonResponse({
+                "error": f"User with email {email} does not exist."
+            }, status=400)
+
+    # Get contents of email
+    subject = data.get("subject", "")
+    body = data.get("body", "")
+
+    # Create one email for each recipient, plus sender
+    users = set()
+    users.add(request.user)
+    users.update(recipients)
+    for user in users:
+        email = Post(
+            user=user,
+            sender=request.user,
+            subject=subject,
+            body=body,
+            read=user == request.user
+        )
+        email.save()
+        for recipient in recipients:
+            email.recipients.add(recipient)
+        email.save()
+
+    return JsonResponse({"message": "Post sent successfully."}, status=201)
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "mail/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "mail/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "mail/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "mail/register.html", {
+                "message": "Post address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "mail/register.html")
+
+
